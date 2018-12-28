6e987c586281715572beee3da8d89eb96f6a9892
Joey Lam <joeylamcy@gmail.com>
2018-12-28 18:02:16 +0800

    Add Python Lab1

---
+++ misc/sample_quad_eq.py
@@ -0,0 +1,18 @@
+# Solve the quadratic equation ax**2 + bx + c = 0
+
+# import library
+import numpy as np
+
+# set the values of coefficients
+a = 1
+b = 5
+c = 6
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-np.sqrt(d))/(2*a)
+sol2 = (-b+np.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1,sol2))
