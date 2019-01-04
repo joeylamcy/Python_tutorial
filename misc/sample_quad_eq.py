df3f7dc364aa4d96d520ebcc0108f32dd6857a39
Joey Lam <joeylamcy@gmail.com>
2019-01-04 18:31:32 +0800

    edit part 2 & 3

--- misc/sample_quad_eq.py
+++ misc/sample_quad_eq.py
@@ -15,4 +15,4 @@ d = (b**2) - (4*a*c)
 sol1 = (-b-np.sqrt(d))/(2*a)
 sol2 = (-b+np.sqrt(d))/(2*a)
 
-print('The solution are {0} and {1}'.format(sol1,sol2))
+print('The solutions are {0} and {1}'.format(sol1,sol2))
